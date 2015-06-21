db1d8116ab9fe362fe946b28a792902a9f8ba836
Mike Higgins <mrmikehiggins@gmail.com>
2015-06-21 23:28:41 +0800

    added git_ignore and Readme

---
+++ run_analysis.R
@@ -0,0 +1,19 @@
+run_analysis <- function() {
+
+# Information on data set
+# Abstract: Human Activity Recognition database built from the recordings of 30 subjects
+# performing activities of daily living (ADL) while carrying a waist-mounted smartphone 
+# with embedded inertial sensors.
+# Source: http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+
+# Download data file
+# dataurl <- 'https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'
+# destfile <- 'UCI-HAR-Dataset.zip'
+# download.file(dataurl, destfile, "curl")
+
+# First, load the two datasets into memory
+
+
+
+
+}
